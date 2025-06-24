c4aca0a816a4669c8afab7e0938fffe36aea5129
Khofar.github.io <123445728+Khofar@users.noreply.github.com>
2025-06-24 15:59:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
-}
+<script>
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+      pageLanguage: 'en',
+      autoDisplay: false
+    }, 'google_translate_element');
+  }
 
-// Dropdown language switch
-document.getElementById('languageSelect').addEventListener('change', function() {
-  const lang = this.value;
-  const currentUrl = window.location.origin + window.location.pathname;
-  const newUrl = currentUrl + '#googtrans(en|' + lang + ')';
-  window.location.href = newUrl;
-  setTimeout(() => {
-    window.location.reload();
-  }, 100);
-});
-
-// Load Google Translate script
-(function() {
-  const gtScript = document.createElement('script');
-  gtScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-  document.body.appendChild(gtScript);
-})(); 
+  // Load Google Translate script
+  (function () {
+    const script = document.createElement('script');
+    script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    document.body.appendChild(script);
+  })();
+</script>
